cf9c38f7f9b48bf8da6e5cc0cdc9d8bf8d8b7813
08shiva <shivacsepsg@gmail.com>
2024-09-16 13:57:57 +0530

    shiva la commit

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: depend_on_referenced_packages, use_super_parameters
 
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/adm-login.dart';
 import 'package:flutter_frontend/login.dart';
 import 'package:flutter_frontend/signup.dart';
 import 'package:flutter_frontend/dummy.dart';
@@ -20,11 +21,12 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SignupPage(),
+        home: AdminLoginPage(),
         routes: {
           '/login': (context) => LoginPage(),
           '/signup': (context) => SignupPage(),
           '/dummy': (context) => MyHomePage(),
+          '/adm-login':(context)=>AdminLoginPage()
           /*'/pref1':(context) => Preference1(),
         '/pref2':(context) => Preference2(),
         '/mainpage':(context) => MainMusicPage(),
