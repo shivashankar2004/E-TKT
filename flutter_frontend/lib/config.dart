8df89be0097ceb8a67cc85139e361576df04021f
MK4 <mithunkarthik123@gmail.com>
2024-10-20 17:15:17 +0530

    up

--- flutter_frontend/lib/config.dart
+++
@@ -1,7 +0,0 @@
-const url = 'https://e-tkt.onrender.com/';
-const registeration = "${url}register";
-const String wlcm = "Welcome to our ";
-const String nxt = "E-Ticket Booking APP";
-const String signup = "Sign up";
-const String login = "Log in";
-
