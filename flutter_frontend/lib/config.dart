9be542fe85890c91d4ba77815f5eae223c761325
08shiva <shivacsepsg@gmail.com>
2024-10-20 17:37:13 +0530

    xml

--- flutter_frontend/lib/config.dart
+++ flutter_frontend/lib/config.dart
@@ -1,4 +1,4 @@
-const url = 'http://192.168.121.176:5555/';
+const url = 'https://e-tkt.onrender.com/';
 const registeration = "${url}register";
 const String wlcm = "Welcome to our ";
 const String nxt = "E-Ticket Booking APP";
